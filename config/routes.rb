3001955450f735e9acba999bc1853210f09b82fa
Hugo Veeger <hugo.veeger@defacto.nl>
2016-11-17 16:21:43 +0100

    setup all the stuff

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  root to: 'pages#index'
+
+  resources :users, only: [:new, :create]
+  resources :sessions, only: [:new, :create]
+
+  delete '/logout', to: 'sessions#destroy', as: :logout
+end
